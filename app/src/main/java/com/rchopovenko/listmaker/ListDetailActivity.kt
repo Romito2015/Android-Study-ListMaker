1bce11ea7eba50548f7a2113f0117731e1fd3d70
Roman Chopovenko <rchopovenko@vinditek.com>
2018-06-21 17:21:24 +0300

    second activity added

--- app/src/main/java/com/rchopovenko/listmaker/ListDetailActivity.kt
+++ app/src/main/java/com/rchopovenko/listmaker/ListDetailActivity.kt
@@ -2,15 +2,22 @@ package com.rchopovenko.listmaker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class ListDetailActivity : AppCompatActivity() {
 
     lateinit var list: TaskList
+    lateinit var listItemSRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_detail)
         list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)
         title = list.name
+
+        listItemSRecyclerView = findViewById<RecyclerView>(R.id.list_items_recycler_view)
+        listItemSRecyclerView.layoutManager = LinearLayoutManager(this)
+        listItemSRecyclerView.adapter = ListItemsRecyclerViewAdapter(list)
     }
 }
